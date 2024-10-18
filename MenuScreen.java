a42f40bced07f47ead9fb9dee3e40712542daf6b
ilikecandy <jacobnaonao7@gmail.com>
2024-10-18 12:24:15 -0400

    Add high scores - map

--- MenuScreen.java
+++ MenuScreen.java
@@ -1,8 +1,12 @@
 import greenfoot.*;
+import java.util.Map;
+import java.util.HashMap;
 
 public class MenuScreen extends World {
     private Queue<GreenfootImage> avatarImages = new Queue<>();
     private Avatar avatar;
+    
+    private Map<String, Integer> scores = new HashMap<>();
     
     public MenuScreen() {
         super(600, 400, 1);
@@ -21,6 +25,22 @@ public class MenuScreen extends World {
         
         //avatar button
         addObject(new Button(this::nextAvatar), 300, 200);
+        
+        // high scores
+        scores.put("name 1", 100);
+        scores.put("name 2", 200);
+        scores.put("Jacob", 10000);
+        
+        // display high scores
+        int offsetY = 0;
+        for (Map.Entry<String, Integer> entry : scores.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
+            
+            addObject(new Label(key + ": " + value, 30), 100, 200 - offsetY);
+            offsetY += 25;
+        }
+        
     }
 
     public void goInstructions() {
